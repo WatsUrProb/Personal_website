7b92e3449da1b9ff702340c2bcfefe42abdd14ae
WatsUrProb <jovanong04@yahoo.com.sg>
2025-06-19 21:09:17 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+var typed = new Typed(".text",{
+    strings:["Frontend Developer","Web Developer","NUS Student","Learner"],
+    typeSpeed:75,
+    backSpeed: 75,
+    backDelay: 1000,
+    loop: true
+})
+
+
+/*Thing to accomplish:
+1) Frame sizing
+2) Direct to Dsta hackathon github
+3) Attach resume to button
+4) Add snippets to project div
+5) add gradient to website*/
